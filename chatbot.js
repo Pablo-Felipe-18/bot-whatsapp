4274f0fe0c736e422cd8ec82d7d2376028087c91
Pablo Felipe <pfsa1810@hotmail.com>
2025-05-29 17:01:40 -0300

    ajuste puppeteer cloud

--- chatbot.js
+++ chatbot.js
@@ -78,7 +78,13 @@ function registrarNaPlanilha(protocolo, nome, numero, mensagem, origem = 'Client
 
 const qrcode = require('qrcode-terminal');
 const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
